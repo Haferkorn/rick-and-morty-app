889b61b332ef4e583d6d0092cb89b19681c1227a
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-09-29 18:09:00 +0200

    Add Flex Container in Body

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import CharacterCard from "./Components/Body/CharacterCard";
 import Header from "./Components/Header/Header";
 import characterResponse from "./Data/character-response.json"
+import Body from "./Components/Body/Body";
 
 
 function App() {
@@ -11,10 +12,7 @@ function App() {
   return (
     <div className="App">
         <Header title="Rick and Morty API - Galery" subtitle="Anette Haferkorn"/>
-        <CharacterCard character={characters[0]}/>
-        <CharacterCard character={characters[1]}/>
-        <CharacterCard character={characters[2]}/>
-        <CharacterCard character={characters[3]}/>
+        <Body characters={characters}/>
     </div>
   );
 }
--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,7 +1,13 @@
+import CharacterCard from "./CharacterCard";
+import "./Body.css"
 
 function Body(props){
     return(
-        <div>
+        <div className="card__Container">
+            <CharacterCard character={props.characters[0]}/>
+            <CharacterCard character={props.characters[1]}/>
+            <CharacterCard character={props.characters[2]}/>
+            <CharacterCard character={props.characters[3]}/>
 
         </div>
     )
