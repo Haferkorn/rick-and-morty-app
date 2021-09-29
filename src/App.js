e1a340a9d53a73eb6cad18aab249d6e06a26272a
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-09-29 17:22:08 +0200

    Add Rick and Morty API Data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
+import CharacterCard from "./Components/CharacterCard";
 import Header from "./Components/Header";
+import characterResponse from "./Data/character-response.json"
 
 function App() {
     const name="Anette"
+    const characters=characterResponse.results;
   return (
     <div className="App">
         <Header title="Rick and Morty API - Galery" subtitle="Anette Haferkorn"/>
+        <CharacterCard character={characters[0]}/>
     </div>
   );
 }
