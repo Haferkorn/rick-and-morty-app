ae67e0a9e11dd76dde2a4e9c3b8cf5135a21cce9
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-09-30 12:37:40 +0200

    Add on Click Character loading with useState

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import CharacterCard from "./Components/Body/CharacterCard";
 import Header from "./Components/Header/Header";
 import characterResponse from "./Data/character-response.json"
 import Gallery from "./Components/Body/Gallery";
+import {useState} from "react";
 
 
 function App() {
-    const name="Anette"
-    const characters=characterResponse.results;
+
+    //const characters=characterResponse.results;
+
+    const [characters, setcharacters]=useState([])
+
+    const handeleButtonClick=()=>{
+        setcharacters(characterResponse.results)
+    }
   return (
     <div className="App">
         <Header title="Rick and Morty API - Galery" subtitle="Anette Haferkorn"/>
+        <button onClick={handeleButtonClick}>Load Characters</button>
         <Gallery characters={characters}/>
     </div>
   );
--- src/Components/Body/Gallery.js
+++ src/Components/Body/Gallery.js
@@ -8,14 +8,13 @@ function Gallery(props){
             {
                 props.characters.map(character => <CharacterCard key={character.id} character={character}/>)
             }
-
             {/* Map replaces separate calls
-
             <CharacterCard character={props.characters[0]}/>
             <CharacterCard character={props.characters[1]}/>
             <CharacterCard character={props.characters[2]}/>
             <CharacterCard character={props.characters[3]}/>
             */}
+
         </div>
     )
 }export default Gallery
