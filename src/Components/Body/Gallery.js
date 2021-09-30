cdb4c1d85bc749ff28562ce56a269b32024abb9d
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-09-30 11:11:22 +0200

    Add Map for Character Cards

--- src/Components/Body/Gallery.js
+++ src/Components/Body/Gallery.js
@@ -4,11 +4,18 @@ import "./Galery.css"
 function Gallery(props){
     return(
         <div className="card__Container">
+
+            {
+                props.characters.map(character => <CharacterCard key={character.id} character={character}/>)
+            }
+
+            {/* Map replaces separate calls
+
             <CharacterCard character={props.characters[0]}/>
             <CharacterCard character={props.characters[1]}/>
             <CharacterCard character={props.characters[2]}/>
             <CharacterCard character={props.characters[3]}/>
-
+            */}
         </div>
     )
 }export default Gallery
