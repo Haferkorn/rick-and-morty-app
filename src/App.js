9d7187ee8466e15211389eb60f1888a4887ee14c
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-09-30 15:36:53 +0200

    Add Filter

--- src/App.js
+++ src/App.js
@@ -12,15 +12,50 @@ function App() {
     //const characters=characterResponse.results;
 
     const [characters, setcharacters]=useState([])
+    const [search, setSearch] = useState("")
+    const [species, setSpecies]=useState({
+        Human: true,
+        Alien: true
 
-    const handeleButtonClick=()=>{
+        })
+
+    const loadAllCharacters=()=>{
         setcharacters(characterResponse.results)
     }
-  return (
+    const loadRandomCharacter=()=>{
+        setcharacters([characterResponse.results[Math.floor(Math.random() * (19 - 0 + 1) + 0)]])
+    }
+    const clearAllCharacters=()=>{
+        setcharacters([])
+    }
+
+    const handleInput = event => {
+        const newSearch = event.target.value
+        setSearch(newSearch)
+    }
+
+    function handleChangeonSpeciesState(evt) {
+        const value = evt.target.type === "checkbox" ? evt.target.checked : evt.target.value;
+        setSpecies({...species, [evt.target.name]: value});
+        }
+
+    const filteredCharacters= characters.filter(character =>
+            character.name.toLowerCase().includes(search.toLowerCase())
+       )
+
+
+    return (
     <div className="App">
         <Header title="Rick and Morty API - Galery" subtitle="Anette Haferkorn"/>
-        <button onClick={handeleButtonClick}>Load Characters</button>
-        <Gallery characters={characters}/>
+        <button onClick={loadAllCharacters}>Load All Characters</button>
+        <button onClick={loadRandomCharacter}>Load Random Characters</button>
+        <input type="text" onChange={handleInput} value={search} />
+        <input type="checkbox" name="Human" checked={species.Human} onChange={handleChangeonSpeciesState}/>
+        <label>Human</label>
+        <input type="checkbox" name="Alien" checked={species.Alien} onChange={handleChangeonSpeciesState}/>
+        <label>Alien</label>
+        <button onClick={clearAllCharacters}>Clear All</button>
+        <Gallery characters={filteredCharacters}/>
     </div>
   );
 }
