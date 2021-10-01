5fae6ab2fa8c71b6426761209f6768bbc807000f
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-10-01 12:20:30 +0200

    add API Fetch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 
 import './App.css';
-import CharacterCard from "./Components/Body/CharacterCard";
 import Header from "./Components/Header/Header";
-import characterResponse from "./Data/character-response.json"
 import Gallery from "./Components/Body/Gallery";
 import {useEffect, useState} from "react";
 import {fetchCharacters} from "./services/rick-and-morty-ap-service";
 
 
 function App() {
-
-    //const characters=characterResponse.results;
 
     const [characters, setcharacters]=useState([])
 
@@ -23,34 +19,20 @@ function App() {
     },[])
 
 
-    const loadAllCharacters=()=>{
-        setcharacters(characterResponse.results)
-    }
-    const loadRandomCharacter=()=>{
-        setcharacters([characterResponse.results[Math.floor(Math.random() * (19 - 0 + 1) + 0)]])
-    }
-    const clearAllCharacters=()=>{
-        setcharacters([])
-    }
-
     const handleSearch = (event) => {
         const search = event.target.value;
-        setcharacters(characterResponse.results.filter((character) =>
+        setcharacters(characters.filter((character) =>
             character.name
                 .toLowerCase()
                 .includes(search.toLowerCase())
         ));
     }
 
-
     return (
     <div className="App">
         <Header title="Rick and Morty API - Galery" subtitle="Anette Haferkorn"/>
         <div className="button__container">
-            <button onClick={loadAllCharacters}>Load All Characters</button>
-            <button onClick={loadRandomCharacter}>Load Random Characters</button>
             <input type="text" onChange={handleSearch} placeholder="Search for Character Name:"/>
-            <button onClick={clearAllCharacters}>Clear All</button>
         </div>
         <Gallery characters={characters}/>
     </div>
